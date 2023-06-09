ed9163daccc8803d0ab84f929825db15d2fa7772
Your Name <you@example.com>
2023-06-09 16:54:02 +0400

    version1

---
+++ Content.jsx
@@ -0,0 +1,19 @@
+import './style.css';
+
+
+function Content() {
+     return (
+          <div className='content-container'>
+               <img src="https://rlemasquerier.github.io/react.svg" alt="" />
+               <div>
+                    <h2> Lorem ipsum dolor sit.</h2>
+                    <hr />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt odio tenetur inventore laudantium? Inventore laborum, asperiores similique quidem cupiditate nemo incidunt quisquam tempore non, consequuntur, repellat hic earum tempora autem exercitationem? Tempore dolorem ducimus perspiciatis possimus exercitationem ipsa nostrum perferendis? Voluptatibus voluptatum unde explicabo, esse at saepe animi, nam doloremque distinctio sit ducimus laboriosam deserunt aut labore cum. Quaerat fugiat cupiditate, aspernatur doloribus hic aliquam voluptate dolores alias! Excepturi quas suscipit vel, magnam autem eum.</p>
+               </div>
+          </div>
+
+     )
+
+}
+
+export default Content;
